cf09b1c534eab6222648f8e0089686609bf33411
John Robinson <johnrobinsn@gmail.com>
2025-01-18 10:36:08 -0500

    include plugins

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='askit',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(include=['askit', 'askit.*']),
     install_requires=[
         'aiohttp',
         'python-dotenv',
